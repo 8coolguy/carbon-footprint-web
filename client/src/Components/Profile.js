7346f2ad54d2f397ae5613cd3762ec9d743ed50b
Arnav Choudhury <66573183+8coolguy@users.noreply.github.com>
2022-09-18 22:48:21 -0700

    ui changes

--- client/src/Components/Profile.js
+++ client/src/Components/Profile.js
@@ -69,8 +69,8 @@ class Profile extends React.Component{
     render(){
         return(
             <Row className="justify-content-md-center">
-                <Col md={8}>
-                    <Card className="justify-content-md-center">
+                <Col className="profile_container" md={4}>
+                    <Card className="profile">
                     {this.state.user.photoURL?<img src={this.state.user.photoURL} referrerPolicy="no-referrer" alt="" width="120" height="120" className="justify-content-md-center rounded-circle me-2"></img>:<img src="https://www.nicepng.com/png/detail/73-730154_open-default-profile-picture-png.png" referrerPolicy="no-referrer" alt="" width="120" height="120" className="rounded-circle me-2"></img>}
                     {this.state.user.displayName?<h3>{this.state.user.displayName}</h3>:<h3>{this.state.user.email}</h3>}
                     <p>Household Size:</p>
